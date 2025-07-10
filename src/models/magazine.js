ae97ef963904025a292b7377802686970d8de3e4
Yurdoy <yurdoy@gmail.com>
2025-07-10 09:08:39 +0200

    publisher model created

---
+++ src/models/magazine.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const publisherSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Publisher = mongoose.model("Publisher", publisherSchema);

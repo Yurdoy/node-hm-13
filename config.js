0d48e56d9c714096ad492959b1588284f8acf566
Yurdoy <yurdoy@gmail.com>
2025-07-10 09:33:34 +0200

    config.js created

---
+++ config.js
@@ -0,0 +1,14 @@
+import mongoose, { connect } from "mongoose";
+
+const MONGO_URI = process.env.MONGO_URI;
+async function connectDb() {
+  try {
+    const connection = await mongoose.connect(MONGO_URI);
+    console.log("MongoDb connected", connection.connection.host);
+  } catch (error) {
+    console.error(error);
+    process.exit();
+  }
+}
+
+export default connectDb;
